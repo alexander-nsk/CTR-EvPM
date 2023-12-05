31c579b0d7241dd42fc1a2a3dde467065a4b2e72
aleksandr-dudko <aleksandr.dudko@akvelon.com>
2023-12-05 23:55:34 +0400

    Init test task

---
+++ src/main/java/com/allmagen/testtask/HomeController.java
@@ -0,0 +1,15 @@
+package com.allmagen.testtask;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Home redirection to OpenAPI api documentation
+ */
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String index() {
+        return "redirect:api-docs/";
+    }
+}
---
+++ src/main/java/com/allmagen/testtask/TestTaskApplication.java
@@ -0,0 +1,16 @@
+package com.allmagen.testtask;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@OpenAPIDefinition(
+		info = @Info(title = "Test Task", version = "0.0.1", description = "Test API Documentation.")
+)
+@SpringBootApplication
+public class TestTaskApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TestTaskApplication.class, args);
+	}
+}
