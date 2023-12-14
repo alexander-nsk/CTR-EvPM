f9b225a314204000cdefe728cafe313365e3e2eb
aleksandr-dudko <aleksandr.dudko@akvelon.com>
2023-12-14 17:18:09 +0400

    Add first db test.

---
+++ src/test/java/com/allmagen/testtask/ActionRepositoryTest.java
@@ -0,0 +1,8 @@
+package com.allmagen.testtask;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ActionRepositoryTest extends CommonRepositoryTest {
+
+}
---
+++ src/test/java/com/allmagen/testtask/CommonRepositoryTest.java
@@ -0,0 +1,66 @@
+package com.allmagen.testtask;
+
+import com.allmagen.testtask.model.ActionEntity;
+import com.allmagen.testtask.model.ViewEntity;
+import com.allmagen.testtask.repository.ActionRepository;
+import com.allmagen.testtask.repository.ViewRepository;
+import jakarta.transaction.Transactional;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = TestTaskApplication.class)
+@ActiveProfiles("tc")
+@ContextConfiguration(initializers = {CommonRepositoryTest.Initializer.class})
+public class CommonRepositoryTest {
+    @Autowired
+    protected ActionRepository actionRepository;
+    @Autowired
+    private ViewRepository viewRepository;
+    @ClassRule
+    public static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer("postgres:15-alpine3.17")
+            .withDatabaseName("mydb")
+            .withUsername("myuser")
+            .withPassword("mypas");
+
+    @Test
+    @Transactional
+    public void givenViewsInDB_WhenCheckViewsNumber() {
+        ViewEntity viewEntity1 = new ViewEntity();
+        viewEntity1.setUid("1");
+
+        ViewEntity viewEntity2 = new ViewEntity();
+        viewEntity2.setUid("2");
+
+        viewRepository.save(viewEntity1);
+        viewRepository.save(viewEntity2);
+
+        long count = viewRepository.count();
+
+        assertThat(count).isEqualTo(2);
+    }
+
+    static class Initializer
+            implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+            TestPropertyValues.of(
+                    "spring.datasource.url=" + postgreSQLContainer.getJdbcUrl(),
+                    "spring.datasource.username=" + postgreSQLContainer.getUsername(),
+                    "spring.datasource.password=" + postgreSQLContainer.getPassword()
+            ).applyTo(configurableApplicationContext.getEnvironment());
+        }
+    }
+}
